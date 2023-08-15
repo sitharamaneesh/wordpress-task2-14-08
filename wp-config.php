677bbe110da8f22f52ba3d9a1d6b69644b543056
SITHARA MANEESH <91305911+sitharamaneesh@users.noreply.github.com>
2023-08-15 13:44:36 +0530

    Update wp-config.php

--- wp-config.php
+++ wp-config.php
@@ -20,16 +20,16 @@
 
 // ** Database settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define( 'DB_NAME', 'wordpress_database' );
+define( 'DB_NAME', 'wordpress' );
 
 /** Database username */
-define( 'DB_USER', 'root' );
+define( 'DB_USER', 'user' );
 
 /** Database password */
 define( 'DB_PASSWORD', 'wordpress' );
 
 /** Database hostname */
-define( 'DB_HOST', '34.28.103.233' );
+define( 'DB_HOST', 'host' );
 
 /** Database charset to use in creating database tables. */
 define( 'DB_CHARSET', 'utf8mb4' );
